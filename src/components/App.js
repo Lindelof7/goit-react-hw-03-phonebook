5b21e68b13476587d3fdc08a0dd82e38e0df9765
Volodymyr Butyter <butytervolodia@gmail.com>
2023-02-25 20:18:13 +0200

    deploy bug

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { React, Component } from "react";
 import PropTypes from 'prop-types'
 import nextId from "react-id-generator";
 import { ContactList } from './ContactList'
