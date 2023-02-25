37a2b08df1dba94a2a54b24ba9ce263f5cd1bf16
Volodymyr Butyter <butytervolodia@gmail.com>
2023-02-25 20:05:09 +0200

    bug fix

--- src/components/App.js
+++ src/components/App.js
@@ -14,7 +14,11 @@ export class App extends Component {
   };
 
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' }],
     name: PropTypes.string.isRequired,
     filter: '',
   }
@@ -44,7 +48,6 @@ export class App extends Component {
     if (findSame) {
       return Report.failure('You have already added this contact')
     }
-    console.log(this.state.name.length)
 
     this.setState({
       contacts: [...this.state.contacts, newContact]
