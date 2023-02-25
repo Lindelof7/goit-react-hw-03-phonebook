634d55dad45c4e6065e0fd40ebe891e499041099
Volodymyr Butyter <butytervolodia@gmail.com>
2023-02-25 20:15:24 +0200

    trying to fix delpoy bug

--- src/components/App.js
+++ src/components/App.js
@@ -23,19 +23,15 @@ export class App extends Component {
     filter: '',
   }
 
-  // componentDidMount() {
-  //   this.setState({
-  //     contacts: JSON.parse(localStorage.getItem('contacts'))
-  //   })
-  // }
+  componentDidMount() {
+    this.setState({
+      contacts: JSON.parse(localStorage.getItem('contacts'))
+    })
+  }
 
-  // componentDidUpdate(prevProps, prevState) {
-  //   localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
-  // }
-
-
-
-
+  componentDidUpdate(prevProps, prevState) {
+    localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
+  }
 
 
   addContact = (evt) => {
