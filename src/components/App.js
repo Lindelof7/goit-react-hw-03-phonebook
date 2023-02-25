3d10ccf41c1228b10084b0b023632dcd745c2543
Volodymyr Butyter <butytervolodia@gmail.com>
2023-02-25 20:07:25 +0200

    trying to fix deploy bug

--- src/components/App.js
+++ src/components/App.js
@@ -23,15 +23,20 @@ export class App extends Component {
     filter: '',
   }
 
-  componentDidMount() {
-    this.setState({
-      contacts: JSON.parse(localStorage.getItem('contacts'))
-    })
-  }
+  // componentDidMount() {
+  //   this.setState({
+  //     contacts: JSON.parse(localStorage.getItem('contacts'))
+  //   })
+  // }
 
-  componentDidUpdate(prevProps, prevState) {
-    localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
-  }
+  // componentDidUpdate(prevProps, prevState) {
+  //   localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
+  // }
+
+
+
+
+
 
   addContact = (evt) => {
     evt.preventDefault();
