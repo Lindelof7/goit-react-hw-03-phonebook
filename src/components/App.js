510e1ba2dcab1606a7c64087ae50b98e702fba6a
Volodymyr Butyter <butytervolodia@gmail.com>
2023-02-25 20:19:55 +0200

    bug fix

--- src/components/App.js
+++ src/components/App.js
@@ -23,11 +23,6 @@ export class App extends Component {
     filter: '',
   }
 
-  componentDidMount() {
-    this.setState({
-      contacts: JSON.parse(localStorage.getItem('contacts'))
-    })
-  }
 
   componentDidUpdate(prevProps, prevState) {
     localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
